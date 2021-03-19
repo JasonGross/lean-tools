f7582cbc0a94220ce2128681f3a87e0532b399be
Jason Gross <jgross@mit.edu>
2021-03-19 16:50:00 -0400

    Start trying to run lean on files

--- LeanTools/FindBug.lean
+++ LeanTools/FindBug.lean
@@ -1,14 +1,27 @@
 import Lean.Elab.Import
 import Lean.Elab.Command
 import Lean.Util.Profile
---import Cli
+import Lean.Elab.Frontend
+import Init.System.IO
+-- import Cli
 
 namespace LeanTools.FindBug
+
+def runLean (input : String) (opts : Lean.Options) (fileName : String) : IO (Lean.Environment × Lean.MessageLog) := open Lean in do
+  let inputCtx := Parser.mkInputContext input fileName
+  let (header, parserState, messages) ← Parser.parseHeader inputCtx
+  let (env, messages) ← Elab.processHeader header opts messages inputCtx
+  let s ← Elab.IO.processCommands inputCtx parserState (Elab.Command.mkState env messages opts)
+  pure (s.commandState.env, s.commandState.messages)
+
 def run' (args : List String) : IO UInt32 := do
   IO.println args
   match args with
   | inputFileName :: outputFileName :: rest_args =>
-
+    let contents ← IO.FS.readFile inputFileName
+    let (env, msgs) ← runLean contents Lean.Options.empty inputFileName
+    for msg in msgs.toList do
+      IO.print (← msg.toString (includeEndPos := true))
     pure 0
   | _ =>
     IO.println s!"Not enough arguments {args}"
@@ -20,8 +33,8 @@ section
 /-
 open Cli
 def cmd := `[Cli|
-  findbug VIA run; ["0.0.1"]
-  "findbug attempts to create a small file which reproduces a bug found in a large development."
+  "find-bug" VIA run; ["0.0.1"]
+  "find-bug attempts to create a small file which reproduces a bug found in a large development."
 ]
 -/
 end
