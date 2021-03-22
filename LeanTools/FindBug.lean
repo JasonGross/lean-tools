0d1bfbcb9e93936a36840de5b352e3e13d2f9623
Jason Gross <jgross@mit.edu>
2021-03-22 10:42:50 -0400

    Partial work on parsing some basic arguments
    Currently testing with
    ```
    leanpkg build bin LINK_OPTS=-rdynamic; (cd syntax && ../../lean-tools/build/bin/LeanTools find-bug Syntax/Procat.lean Syntax/bug_01.lean --base-dir=. --lean-path="/home/jgross/.elan/toolchains/leanprover-lean4-nightly/lib/lean/:././build" --max-error-count=1)
    ```

--- LeanTools/FindBug.lean
+++ LeanTools/FindBug.lean
@@ -2,10 +2,48 @@ import Lean.Elab.Import
 import Lean.Elab.Command
 import Lean.Util.Profile
 import Lean.Elab.Frontend
+import Lean.Server.Snapshots
 import Init.System.IO
 -- import Cli
 
+def List.takeOpt : Option Nat → List α → List α
+  | Option.none, ls => ls
+  | Option.some n, ls => ls.take n
+
 namespace LeanTools.FindBug
+
+structure Options where
+  lean_path : List String := []
+  init_search_path : Bool := true
+  max_error_count : Option Nat := Option.none
+
+def Options.default : Options := { }
+
+deriving instance Repr for Options
+
+instance : ToString Options where
+  toString (o : Options) := s!"{repr o}"
+
+def parse_options : List String → Options × List String
+  | [] => ({ }, [])
+  | arg :: args =>
+    match arg.splitOn "=" with
+    | "--lean-path" :: rest =>
+      let (o, args) := parse_options args
+      ({o with lean_path := ("=".intercalate rest)::o.lean_path}, args)
+    | ["--no-init-search-path"] =>
+      let (o, args) := parse_options args
+      ({o with init_search_path := false }, args)
+    | ["--max-error-count", n] =>
+      let (o, args) := parse_options args
+      match n.toNat? with
+      | Option.some n =>
+        ({o with max_error_count := n}, args)
+      | Option.none =>
+        (o, arg::args)
+    | _ =>
+      let (o, args) := parse_options args
+      (o, arg :: args)
 
 def runLean (input : String) (opts : Lean.Options) (fileName : String) : IO (Lean.Environment × Lean.MessageLog) := open Lean in do
   let inputCtx := Parser.mkInputContext input fileName
@@ -18,10 +56,16 @@ def run' (args : List String) : IO UInt32 := do
   IO.println args
   match args with
   | inputFileName :: outputFileName :: rest_args =>
-    Lean.initSearchPath Option.none
+    let (opts, rest_args) := parse_options rest_args
+    IO.println opts
+    IO.println rest_args
+    let sp := match opts.lean_path with
+      | [] => Option.none
+      | _ => Option.some ([System.FilePath.searchPathSeparator].asString.intercalate opts.lean_path)
+    if opts.init_search_path then Lean.initSearchPath sp else pure ()
     let contents ← IO.FS.readFile inputFileName
     let (env, msgs) ← runLean contents Lean.Options.empty inputFileName
-    for msg in msgs.toList do
+    for msg in msgs.toList.takeOpt opts.max_error_count do
       IO.print (← msg.toString (includeEndPos := true))
     pure 0
   | _ =>
